6ca949936782b9bc9f35a498ae9d1ae9ff995849
Audemars Odato <audemarsodato@gmail.com>
2025-01-19 19:44:19 +0800

    version 0.9.1
    Be able to change timezone

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const currentTime_el = document.getElementById("time");
 const currentDate_el = document.getElementById("date");
-const timezone_el = document.getElementById("timezone");
+const inputTimezone = document.getElementById("timezones");
 
 // usersTimezone
 dayjs.extend(dayjs_plugin_timezone);
 dayjs.extend(dayjs_plugin_utc);
 // const usersTimezone = dayjs.tz.guess();
 // usersTimezone_el.textContent = usersTimezone;
-const TIMEZONE = "Asia/Manila";
 
 setInterval( () => {
+        const TIMEZONE = inputTimezone.value;
         const currentTime = dayjs().tz(TIMEZONE);
-        timezone_el.textContent = TIMEZONE;
         currentTime_el.textContent = currentTime.format('hh:mm:ss');
         currentDate_el.textContent = currentTime.format('dddd, D MMMM, YYYY');
 }, 1000);
