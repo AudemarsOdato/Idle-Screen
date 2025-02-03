c6f18cc7cf7452c0f55c93adb2089eaa82899456
Audemars Odato <audemarsodato@gmail.com>
2025-02-03 10:50:55 +0800

    added dark mode

--- script.js
+++ script.js
@@ -14,4 +14,35 @@ setInterval( () => {
         timezone_el.textContent = TIMEZONE;
         currentTime_el.textContent = currentTime.format('hh:mm:ss');
         currentDate_el.textContent = currentTime.format('dddd, D MMMM, YYYY');
+
+        darkmode(isDarkmode);
 }, 1000);
+
+currentDate_el.addEventListener("click", () => {
+        lightmode();
+});
+
+currentTime_el.addEventListener("click", () => {
+        darkmode();
+});
+
+const body = document.querySelector("body");
+const box = document.querySelector(".box");
+
+function darkmode () {
+        body.classList.add("dark-mode");
+        box.classList.add("dark-mode");
+
+        currentTime_el.style.color = "rgb(228, 228, 228)";
+        currentDate_el.style.color = "rgb(228, 228, 228)";
+        timezone_el.style.color = "rgb(191, 191, 191)";
+}
+
+function lightmode() {
+        body.classList.remove("dark-mode");
+        box.classList.remove("dark-mode");
+
+        currentTime_el.style.color = "rgb(45, 23, 23)";
+        currentDate_el.style.color = "rgb(45, 23, 23)";
+        timezone_el.style.color = "rgba(59, 59, 59, 0.733)";
+}
